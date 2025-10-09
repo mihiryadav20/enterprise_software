2a912abf3c4e32840e69b71741e6aa301cd7ace7
mihiryadav20 <mihir.yadav@somaiya.edu>
2025-10-10 01:13:08 +0530

    migrations and building models

--- enterprise/myapp/models.py
+++ enterprise/myapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True, blank=True)
+    role = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.department}'
