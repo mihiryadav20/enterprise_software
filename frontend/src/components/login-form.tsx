1038b939269bc6d55241e1d8ca83c81c006774e8
mihiryadav20 <mihir.yadav@somaiya.edu>
2025-10-22 18:03:02 +0530

    text

--- frontend/src/components/login-form.tsx
+++ frontend/src/components/login-form.tsx
@@ -59,7 +59,7 @@ export function LoginForm({
     >
       <FieldGroup>
         <div className="flex flex-col items-center gap-1 text-center">
-          <h1 className="text-2xl font-bold">Login to your account</h1>
+          <h1 className="text-sm font-bold">Login to your account</h1>
           <p className="text-muted-foreground text-sm text-balance">
             Enter your email below to login to your account
           </p>
