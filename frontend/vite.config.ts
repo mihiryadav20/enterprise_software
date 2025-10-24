051037367ce6da2bb5eef7d08881d600e97e8ac9
mihiryadav20 <mihir.yadav@somaiya.edu>
2025-10-24 15:51:33 +0530

    react removed, it sucks

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,7 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import tailwindcss from "@tailwindcss/vite"
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [tailwindcss(), sveltekit()]
+});
